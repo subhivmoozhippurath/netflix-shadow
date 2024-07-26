f7dd1874e306f5ed8e2d8f96580558b63ffdd525
subhivmoozhippurath <vmsubhi@gmail.com>
2024-07-26 22:16:38 +0530

    basic setup for netflix shadow react project

---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyAhZmgzj2QDAH0_UvJmYuyFxjJuQsZKFAM",
+    authDomain: "netflix-shadow.firebaseapp.com",
+    projectId: "netflix-shadow",
+    storageBucket: "netflix-shadow.appspot.com",
+    messagingSenderId: "445549751492",
+    appId: "1:445549751492:web:08f3e72c55e08373d06d91"
+  };
